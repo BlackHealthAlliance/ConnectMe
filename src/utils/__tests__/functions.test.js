b6fac2f3557e46fda543123cccae18c9661c77c2
Alexander <36518160+alexander-bo@users.noreply.github.com>
2020-11-14 16:32:34 -0500

    remove obsolete test
    - correct import
    -remove data test

--- src/utils/__tests__/functions.test.js
+++ src/utils/__tests__/functions.test.js
@@ -1,4 +1,5 @@
 import {searchFunction, searchOrganizations} from '../functions';
+import {getCIties, getLanguages} from '../data';
 
 describe('functions - searchFunction', () => {
   beforeEach(() => {
@@ -15,21 +16,6 @@ describe('functions - searchFunction', () => {
   test('It should return a list of languages', () => {
     const response = getLanguages();
     expect(response[0]).toEqual("English");
-  });
-
-  test('It should return an organization by name', () => {
-    const response = getOrganization("Kennedy House Youth Services");
-    console.log(response);
-    // expect(response.crisis).toEqual(false);
-    expect(response.languages[0]).toEqual("English");
-    expect(response.address.city).toEqual("Scarborough");
-    expect(response.address.postalCode).toEqual("A1B2C3");
-    expect(response.address.province).toEqual("ON");
-    expect(response.contact.phone).toEqual("(416) 299-3157");
-    expect(response.contact.email).toEqual("info@kennedyhouse.org");
-    expect(response.contact.web).toEqual("http://www.khys.org/");
-    expect(response.blackLed).toEqual(false);
-    expect(response.services.length).toEqual(4);
   });
   
   /*
