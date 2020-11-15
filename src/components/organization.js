eaeef833de264c927156a26d32ea60624ecb7c80
Benton Hui <benton.hui@gmail.com>
2020-11-15 01:57:22 -0500

    Applied style to organization list as per Figma.

--- src/components/organization.js
+++ src/components/organization.js
@@ -11,15 +11,15 @@ class Organization extends Component {
     
     render = () => 
         <div className="organization">
-            <label htmlFor="name">{this.props.name}</label>
+            <label className="organizationName" htmlFor="name">{this.props.name}</label>
             <br />
             { this.props.isSpotlight ? <img className="spotlightImg" src={spotlightIcon} alt="Pathways to Care"/> : null }
             <br />
-            <label htmlFor="services">{this.props.services.join(", ")}</label>
+            <label className="organizationInfo" htmlFor="services">{this.props.services.join(", ")}</label>
             <br />
-            <label htmlFor="email">{this.props.email}</label>
+            <label className="organizationInfo" htmlFor="email">{this.props.email}</label>
             <br />
-            <label htmlFor="phone">{this.props.phone}</label>
+            <label className="organizationInfo" htmlFor="phone">{this.props.phone}</label>
             <br />
             <a href="{this.props.website}" target="_blank">
                 <input type="button" value="take me to their site" />
